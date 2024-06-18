cd35090d1a995286ce6cdbd8bf86e3afb169919c
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-18 18:04:21 +0300

    .

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """
-Nginx logs in mongodb
+Provide some stats about Nginx logs stored in MongoDB
+Database: logs, Collection: nginx, Display same as example
+first line: x logs, x number of documents in this collection
+second line: Methods
+5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+one line with method=GET, path=/status
 """
 from pymongo import MongoClient
 
