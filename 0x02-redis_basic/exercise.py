a0092fc66c0ede23d6dfd0111c75ffd37a1c3539
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-19 18:17:54 +0300

    Added type conversion functionality

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,17 +2,18 @@
 """
 Redis
 """
-from typing import Union, Callable, Optional
+from typing import Union, Optional, Callable
 import redis
+import uuid
 
 
-class Cache():
+class Cache:
     """
-    Redis
+    Redis Cache
     """
     def __init__(self):
         """
-        class cache init
+        Class cache init
         """
         self._redis = redis.Redis(host="localhost", port=6379, db=0)
         self._redis.flushdb()
@@ -22,8 +23,32 @@ class Cache():
         Generates a random string and uses
         it as key to store data to redis
         """
-        import uuid
-
         random_string = str(uuid.uuid4())
         self._redis.set(random_string, data)
         return random_string
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, float, int, bytes]:
+        """
+        Fetches data from Redis
+        """
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """
+        Conversion function
+        """
+        return str(self._redis.get(key).decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Conversion function
+        """
+        try:
+            return int(self._redis.get(key).decode("utf-8"))
+        except Exception:
+            return 0
