9f66cc0e205d18af817eccb9723e3b42dcf4365d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-19 17:37:25 +0300

    Innitial Commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,16 +10,14 @@ class Cache():
     """
     Redis
     """
-    __redis = None
-
     def __init__(self):
         """
         class cache init
         """
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random string and uses
         it as key to store data to redis
@@ -27,5 +25,5 @@ class Cache():
         import uuid
 
         random_string = str(uuid.uuid4())
-        self.__redis.set(random_string, data)
+        self._redis.set(random_string, data)
         return random_string
